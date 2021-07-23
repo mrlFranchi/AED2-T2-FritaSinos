3c4734a49c1f1de718ec546f7f127c3ea6628f3f
mrlFranchi <murilo.franchi@usp.br>
2021-07-23 14:55:51 -0300

    branch test

--- src/main/java/graph/GraphConverter.java
+++ src/main/java/graph/GraphConverter.java
@@ -3,6 +3,7 @@ package graph;
 public class GraphConverter {
 
     /* TODO: This class*/
+    //this is a branch test
     public static AbstractGraph predecessorListToGraph(AbstractGraph dungeon, int[] predecessor){
         return null;
     }
